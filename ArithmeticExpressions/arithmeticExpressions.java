6439025de26dd6826c212a39a4a2d7fe5fa2cf54
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-11 12:47:53 -0300

    A Expressions

---
+++ ArithmeticExpressions/arithmeticExpressions.java
@@ -0,0 +1,31 @@
+package ArithmeticExpressions;
+
+public class arithmeticExpressions {
+    
+    public static void main(String[] args) {
+
+        // int add = 10 + 10;
+        // int minus = 10 - 5;
+
+        // double result = (double)10 / (double)3;
+
+        // System.out.println(add);
+        // System.out.println(minus);
+        // System.out.println(result);
+
+
+        // int x = 1;
+        // int y = x++;
+        
+
+        // System.out.println(x); // 2
+        // System.out.println(y); // 1
+
+        int x = 1;
+        int y = ++x;
+        
+
+        System.out.println(x); // 2
+        System.out.println(y); // 2
+    }
+}
