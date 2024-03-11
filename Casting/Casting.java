2b50e5f8d16aae43f42b2175896e06227a591831
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-11 12:54:14 -0300

    Casting

---
+++ Casting/Casting.java
@@ -0,0 +1,12 @@
+package Casting;
+
+public class Casting {
+    
+    public static void main(String[] args) {
+
+        String x = "1.1";
+        double y = Double.parseDouble(x) + 2;
+
+        System.out.println(y); // 3.1
+    }
+}
