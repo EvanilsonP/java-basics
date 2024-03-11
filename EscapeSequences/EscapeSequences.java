af08cb7617d53abdafacb92f1372af93a29b9043
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-11 12:15:09 -0300

    Escape Sequences

---
+++ EscapeSequences/EscapeSequences.java
@@ -0,0 +1,11 @@
+package EscapeSequences;
+
+public class EscapeSequences {
+    
+    public static void main(String[] args) {
+
+        String message = "Hello \"Mosh\"";
+
+        System.out.println(message);
+    }
+}
