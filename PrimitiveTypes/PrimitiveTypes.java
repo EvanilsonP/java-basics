59c3624aa8528c15e014d610b423e906f76a0d85
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-11 11:37:21 -0300

    Primitive Types

---
+++ PrimitiveTypes/PrimitiveTypes.java
@@ -0,0 +1,14 @@
+package PrimitiveTypes;
+
+public class PrimitiveTypes {
+    
+    public static void main(String[] args) {
+
+        int age = 25;
+        long viewsCount = 3_123_456L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isElegible = false;
+        
+    }
+}
