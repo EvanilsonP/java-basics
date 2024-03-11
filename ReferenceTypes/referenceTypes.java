eeb97d70d0a10606fc2362e171c68f657b46bf89
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-11 11:42:02 -0300

    Reference Types

---
+++ ReferenceTypes/referenceTypes.java
@@ -0,0 +1,12 @@
+package ReferenceTypes;
+
+import java.util.Date;
+
+public class referenceTypes {
+    public static void main(String[] args) {
+        
+        Date now = new Date();
+        System.out.println(now);
+        
+    }
+}
