58e6341d61bdf8dec423e48060c43ea093c3c46d
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-11 12:36:41 -0300

    Constants

---
+++ Constants/Constants.java
@@ -0,0 +1,12 @@
+package Constants;
+
+public class Constants {
+    
+    public static void main(String[] args) {
+
+        final float PI = 3.14F;
+        // PI = 3; // Cannot be modified
+
+        System.out.println(PI);
+    }
+}
