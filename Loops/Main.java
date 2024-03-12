364a6895d3bd3acd935dfde43788346c751937f5
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-12 13:52:29 -0300

    Loop

---
+++ Loops/Main.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 5; i > 0; i--) {
+            System.out.println("Countdown to happy new year: "+ i);
+        }
+        System.out.println("Happy new year!");
+    }
+}
