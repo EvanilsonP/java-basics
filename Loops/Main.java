7cef5428e4e23393ef4ee25216cd135f716a78fa
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-12 14:03:18 -0300

    While Loops

--- Loops/Main.java
+++ Loops/Main.java
@@ -1,10 +1,22 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for(int i = 5; i > 0; i--) {
-            System.out.println("Countdown to happy new year: "+ i);
+        // for(int i = 5; i > 0; i--) {
+        //     System.out.println("Countdown to happy new year: "+ i);
+        // }
+        // System.out.println("Happy new year!");
+        Scanner scanner = new Scanner(System.in);
+        String message = "";
+
+        while(!message.equals("quit")) {
+            System.out.print("Input: ");
+            message = scanner.nextLine().toLowerCase();
+            System.out.println(message);
         }
-        System.out.println("Happy new year!");
+
+        scanner.close();
     }
 }
