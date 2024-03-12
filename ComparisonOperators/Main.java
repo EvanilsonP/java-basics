cd408629d3a5195529124ec428714895adb3ced7
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-12 14:32:57 -0300

    Comparison Operators

---
+++ ComparisonOperators/Main.java
@@ -0,0 +1,13 @@
+package ComparisonOperators;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 5;
+
+        System.out.println(x == y); // true
+        System.out.println(x != y); // false
+        System.out.println(x >= y); // false
+        System.out.println(x <= y); // true
+    }
+}
