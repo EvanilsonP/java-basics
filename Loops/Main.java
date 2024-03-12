6b1b5ba388e4ed3a9119133cd483858efbe298fb
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-12 14:17:11 -0300

    Break and Continue

--- Loops/Main.java
+++ Loops/Main.java
@@ -20,16 +20,41 @@ public class Main {
 
         // scanner.close();
         
-        String message = "";
+        // String message = "";
 
-        do {
+        // do {
+        //     System.out.print("Input: ");
+        //     message = scanner.nextLine().toLowerCase();
+        //     System.out.println(message);
+            
+            
+        // } while (!message.equals("quit"));
+
+        // scanner.close();
+
+        String input = "";
+
+        while (!input.equals("quit")) {
+
             System.out.print("Input: ");
-            message = scanner.nextLine().toLowerCase();
-            System.out.println(message);
-            
-            
-        } while (!message.equals("quit"));
+            input = scanner.nextLine().toLowerCase();
 
+            // When we quit, the quit word is not returned on the terminal
+            // if(!input.equals("quit")) {
+            //     System.out.println(input);
+            // } 
+            // Goes to the top of the loop and the other if statement is ignored
+            if(input.equals("pass")) {
+                continue;
+            };
+
+            if(input.equals("quit")) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+        
         scanner.close();
     }
 }
